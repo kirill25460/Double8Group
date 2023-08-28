0b9c272136b69ba9ee0c1462a9e1a3f123c63f50
Vlad Melnik <76560283+vmelniki2828@users.noreply.github.com>
2023-08-28 20:51:00 +0300

    small fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,11 @@ import {
   BurgerMenu,
   BurgerIcon,
   SideMenu,
-  CloseBtn
+  CloseBtn,
+  HomeSvg,
+  SideItems,
+  PersonSvg,
+  BookSvg
 } from './Header.styled';
 import { useState } from 'react';
 // MobileOpenHeader,BurgerButton,MobileMenu,MobileCloseHeader,CrossButton
@@ -46,6 +50,12 @@ export const Header = () => {
     { href: '/contacts', text: 'Contacts' },
   ];
 
+  const navSideItems = [
+    { href: `/`, text: 'Home', svg: <HomeSvg /> },
+    { href: '/reviews', text: 'Reviews', svg: <PersonSvg /> },
+    { href: '/book_us', text: 'Book us', svg: <BookSvg /> },
+  ]
+
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -69,17 +79,26 @@ export const Header = () => {
             </div>
           </BurgerMenu>
           <NavList>
-              {navItems.map(({ href, text }) => (
-                <NavItem>
-                  <StyledLink to={href}>
-                    <span>{text}</span>
-                  </StyledLink>
-                </NavItem>
-              ))}
-            </NavList>
+            {navItems.map(({ href, text }) => (
+              <NavItem>
+                <StyledLink to={href}>
+                  <span>{text}</span>
+                </StyledLink>
+              </NavItem>
+            ))}
+          </NavList>
           <SideMenu className={isOpen ? 'open' : ''}>
             <CloseBtn onClick={toggleMenu} />
-            
+            {navSideItems.map(({ href, text, svg }) => {
+              return (
+                <StyledLink to={href}>
+                  <SideItems>
+                    {svg}
+                    {text}
+                  </SideItems>
+                </StyledLink>
+              )
+            })}
           </SideMenu>
         </Wrap>
       </Container>
