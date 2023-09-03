5bac113eb0dc75b81b0f4296cdedad9198415fa4
Vlad Melnik <76560283+vmelniki2828@users.noreply.github.com>
2023-09-03 03:03:37 +0300

    update

--- src/helper/SliderServices.jsx
+++ src/helper/SliderServices.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Modal from './Modal';
-import { height, width } from '@mui/system';
+// import { height, width } from '@mui/system';
 
 export const SliderServices = ({ images, onClose }) => {
     const [currentSlide, setCurrentSlide] = useState(0);
@@ -17,7 +17,7 @@ export const SliderServices = ({ images, onClose }) => {
             <Carousel selectedItem={currentSlide} onChange={setCurrentSlide} showThumbs={false}>
                 {images.map((image, index) => (
                     <div key={index}>
-                        <img src={image.url} alt={`Image ${index}`} style={{width:"100vh", height:"100vh"}}/>
+                        <img src={image.url} alt={`Service ${index}`} style={{width:"100vh", height:"100vh"}}/>
                     </div>
                 ))}
             </Carousel>
