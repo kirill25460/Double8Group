46791b1367f6637dff7d160e547137f7e9748bb1
Vlad Melnik <vmelniki2828@gmail.com>
2023-10-11 00:07:56 +0300

    edit

--- src/components/ReviewsPageComp/OtherReviewsText/OtherReviewsText.styled.jsx
+++ src/components/ReviewsPageComp/OtherReviewsText/OtherReviewsText.styled.jsx
@@ -1,46 +1,41 @@
 import styled from 'styled-components';
 
-
 export const Container = styled.div`
   margin: 0 auto;
-  
   @media screen and (min-width: 768px) {
-
   }
   @media screen and (min-width: 1440px) {
-  
+    display: flex;
+    justify-content: end;
   }
 `;
 
 export const OtherText = styled.h2`
-display: none;
-@media screen and (min-width: 768px) {
-  display: block;
-  margin:0;
-  color:#222021;
-  font-family: Montserrat;
-  font-size: 42px;
-  font-style: normal;
-  font-weight: 800;
-  line-height: 42px;
-  text-align: center;
-}
-@media screen and (min-width: 1440px) {
-  display: block;
-  padding-top:105px;
-padding-bottom:60px;
-padding-left:450px;
-margin:0;
-width:747px;
-  font-size: 56px;
-  line-height: 61px;
-  letter-spacing: 0px;
-  text-align: right;
-}
+  display: none;
+  @media screen and (min-width: 768px) {
+    display: block;
+    margin: 0;
+    color: #222021;
+    font-family: Montserrat;
+    font-size: 42px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: 42px;
+    text-align: center;
+  }
+  @media screen and (min-width: 1440px) {
+    display: block;
+    padding-right: 400px;
+    margin: 0;
+    width: 747px;
+    font-size: 56px;
+    line-height: 61px;
+    letter-spacing: 0px;
+    text-align: right;
+
+  }
 `;
 
-
-
 export const OtherSpan = styled.span`
-color:#F8821E;
+  color: #f8821e;
 `;
